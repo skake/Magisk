c13cec09710c4fc2f5c28d26cdacf45cd2cb548e
Viktor De Pasquale <neytireyqa@gmail.com>
2019-03-16 12:00:45 +0100

    Added binding between viewModel and its states with dummy loading to simulate states

--- manager/src/main/java/com/topjohnwu/magisk/ui/home/HomeViewModel.kt
+++ manager/src/main/java/com/topjohnwu/magisk/ui/home/HomeViewModel.kt
@@ -1,15 +1,32 @@
 package com.topjohnwu.magisk.ui.home
 
+import android.os.Handler
+import androidx.core.os.postDelayed
 import com.skoumal.teanity.databinding.ComparableRvItem
 import com.skoumal.teanity.util.DiffObservableList
+import com.skoumal.teanity.util.KObservableField
 import com.topjohnwu.magisk.BR
 import com.topjohnwu.magisk.model.entity.SupportItem
 import com.topjohnwu.magisk.model.entity.SupportRvItem
 import com.topjohnwu.magisk.ui.base.MagiskViewModel
 import com.topjohnwu.magisk.ui.events.ViewEvent
 import me.tatarka.bindingcollectionadapter2.OnItemBind
+import kotlin.random.Random
 
 class HomeViewModel : MagiskViewModel() {
+
+    object SafetyNetState {
+        const val UNKNOWN = 0
+        const val LOADING = 1
+        const val LOADED = 2
+    }
+
+    val isForceEncryption = KObservableField(false /*fetch default*/)
+    val isAVB = KObservableField(false /*fetch default*/)
+
+    val safetyNetState = KObservableField(SafetyNetState.UNKNOWN)
+    val isCTS = KObservableField(false)
+    val isIntegrity = KObservableField(false)
 
     val items = DiffObservableList(ComparableRvItem.callback).apply {
         val newItems = SupportItem.values().map { SupportRvItem(it) }
@@ -25,5 +42,18 @@ class HomeViewModel : MagiskViewModel() {
     fun installMagiskPressed() {}
     fun installManagerPressed() {}
     fun supportPressed(item: SupportItem) {}
+
+    fun verifyPressed() {
+        safetyNetState.value = SafetyNetState.LOADING
+
+        Handler().postDelayed(3000) {
+            val result = Random(System.currentTimeMillis()).nextBoolean()
+
+            isCTS.value = result
+            isIntegrity.value = result
+
+            safetyNetState.value = SafetyNetState.LOADED
+        }
+    }
 
 }
