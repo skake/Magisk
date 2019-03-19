9d93389bb65f78fbc50fac45a5579e1592e2aa68
Viktor De Pasquale <neytireyqa@gmail.com>
2019-03-19 18:38:12 +0100

    Added tooltips for action icons

--- manager/src/main/java/com/topjohnwu/magisk/util/DataBindingAdapters.kt
+++ manager/src/main/java/com/topjohnwu/magisk/util/DataBindingAdapters.kt
@@ -1,25 +1,20 @@
 package com.topjohnwu.magisk.util
 
 import android.animation.ObjectAnimator
-import android.graphics.drawable.GradientDrawable
-import android.graphics.drawable.InsetDrawable
 import android.view.View
 import android.view.animation.LinearInterpolator
 import android.widget.ImageView
 import androidx.annotation.DrawableRes
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.widget.TooltipCompat
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.Group
 import androidx.core.view.isInvisible
 import androidx.databinding.BindingAdapter
 import androidx.interpolator.view.animation.FastOutSlowInInterpolator
-import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.request.RequestOptions
 import com.skoumal.teanity.databinding.applyTransformation
-import com.skoumal.teanity.extensions.startEndToLeftRight
-import com.skoumal.teanity.extensions.toPx
-import com.skoumal.teanity.util.KItemDecoration
 import com.topjohnwu.magisk.GlideApp
 import com.topjohnwu.magisk.R
 import kotlin.math.roundToInt
@@ -59,51 +54,9 @@ fun setImageResource(view: AppCompatImageView, @DrawableRes resId: Int) {
     view.setImageResource(resId)
 }
 
-@BindingAdapter(
-    "dividerColor",
-    "dividerHorizontal",
-    "dividerSize",
-    "dividerAfterLast",
-    "dividerMarginStart",
-    "dividerMarginEnd",
-    "dividerMarginTop",
-    "dividerMarginBottom",
-    requireAll = false
-)
-fun setDivider(
-    view: RecyclerView,
-    color: Int,
-    horizontal: Boolean,
-    _size: Float,
-    _afterLast: Boolean?,
-    marginStartF: Float,
-    marginEndF: Float,
-    marginTopF: Float,
-    marginBottomF: Float
-) {
-    val orientation = if (horizontal) RecyclerView.HORIZONTAL else RecyclerView.VERTICAL
-    val size = if (_size > 0) _size.roundToInt() else 1.toPx()
-    val (width, height) = if (horizontal) size to 1 else 1 to size
-    val afterLast = _afterLast ?: true
-
-    val marginStart = marginStartF.roundToInt()
-    val marginEnd = marginEndF.roundToInt()
-    val marginTop = marginTopF.roundToInt()
-    val marginBottom = marginBottomF.roundToInt()
-    val (marginLeft, marginRight) = view.context.startEndToLeftRight(marginStart, marginEnd)
-
-    val drawable = GradientDrawable().apply {
-        setSize(width, height)
-        shape = GradientDrawable.RECTANGLE
-        setColor(color)
-    }.let {
-        InsetDrawable(it, marginLeft, marginTop, marginRight, marginBottom)
-    }
-
-    val decoration = KItemDecoration(view.context, orientation)
-        .setDeco(drawable)
-        .apply { showAfterLast = afterLast }
-    view.addItemDecoration(decoration)
+@BindingAdapter("tooltipText")
+fun setTooltip(view: View, tooltip: String) {
+    TooltipCompat.setTooltipText(view, tooltip)
 }
 
 @BindingAdapter("invisibleTY", "invisibleTYMaxAlpha", requireAll = false)
