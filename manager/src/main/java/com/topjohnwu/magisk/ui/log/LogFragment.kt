d81ca18026197c01cded238846039ae7293d3447
Viktor De Pasquale <neytireyqa@gmail.com>
2019-03-21 21:13:24 +0100

    Added a template for logs fragment

--- manager/src/main/java/com/topjohnwu/magisk/ui/log/LogFragment.kt
+++ manager/src/main/java/com/topjohnwu/magisk/ui/log/LogFragment.kt
@@ -1,14 +1,67 @@
 package com.topjohnwu.magisk.ui.log
 
+import android.os.Bundle
+import android.view.View
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.topjohnwu.magisk.R
 import com.topjohnwu.magisk.databinding.FragmentLogBinding
 import com.topjohnwu.magisk.ui.base.MagiskFragment
+import com.topjohnwu.magisk.util.addOnGlobalLayoutListener
+import com.topjohnwu.magisk.util.setOnViewReadyListener
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import kotlin.math.max
 
 
 class LogFragment : MagiskFragment<LogViewModel, FragmentLogBinding>() {
 
     override val layoutRes: Int = R.layout.fragment_log
     override val viewModel: LogViewModel by viewModel()
+
+    private val bottomSheet get() = BottomSheetBehavior.from(binding.logSheet)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setUpSheet()
+        setUpSheetLayout()
+    }
+
+    private fun setUpSheet() {
+        binding.logParent.addOnGlobalLayoutListener {
+            with(binding) {
+                val offset = logParent.measuredHeight - logGlance.bottom
+                bottomSheet.peekHeight = max(100, offset)
+            }
+        }
+    }
+
+    private fun setUpSheetLayout() {
+        bottomSheet.setBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+
+            init {
+                binding.logSheetInclude.sheetInternalLayout.setOnViewReadyListener {
+                    onSlide(binding.logSheet, 0f)
+                }
+            }
+
+            override fun onSlide(view: View, fraction: Float) {
+                with(binding.logSheetInclude) {
+                    sheetInternalLayout.translationY =
+                        sheetAppbar.measuredHeight * (fraction - 1)
+                }
+            }
+
+            override fun onStateChanged(view: View, newState: Int) = Unit
+        })
+    }
+
+    override fun onBackPressed(): Boolean {
+        if (bottomSheet.state == BottomSheetBehavior.STATE_EXPANDED) {
+            binding.logSheetInclude.sheetRecycler.scrollToPosition(0)
+            bottomSheet.state = BottomSheetBehavior.STATE_COLLAPSED
+            return true
+        }
+        return super.onBackPressed()
+    }
 
 }
--- manager/src/main/java/com/topjohnwu/magisk/ui/log/LogViewModel.kt
+++ manager/src/main/java/com/topjohnwu/magisk/ui/log/LogViewModel.kt
@@ -1,6 +1,11 @@
 package com.topjohnwu.magisk.ui.log
 
 import com.topjohnwu.magisk.ui.base.MagiskViewModel
+import com.topjohnwu.magisk.ui.events.ViewEvent
 
 
-class LogViewModel : MagiskViewModel()
+class LogViewModel : MagiskViewModel() {
+
+    fun sheetBackPressed() = ViewEvent.BACK_PRESS.publish()
+
+}
