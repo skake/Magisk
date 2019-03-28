e4a97ae39d06afb99555681b2d580322fabac3b9
Viktor De Pasquale <neytireyqa@gmail.com>
2019-03-28 15:02:28 +0100

    Added an option to control flashing process via volume controls (UP/DOWN) if flashing scrip asks for it

--- manager/src/main/java/com/topjohnwu/magisk/ui/flash/FlashActivity.kt
+++ manager/src/main/java/com/topjohnwu/magisk/ui/flash/FlashActivity.kt
@@ -1,5 +1,6 @@
 package com.topjohnwu.magisk.ui.flash
 
+import android.view.KeyEvent
 import androidx.core.os.bundleOf
 import com.topjohnwu.magisk.R
 import com.topjohnwu.magisk.databinding.ActivityFlashBinding
@@ -14,6 +15,14 @@ class FlashActivity : MagiskActivity<FlashViewModel, ActivityFlashBinding>() {
     override val viewModel: FlashViewModel by viewModel {
         val args = FlashActivityArgs.fromBundle(intent?.extras ?: bundleOf())
         parametersOf(args)
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        return when (keyCode) {
+            KeyEvent.KEYCODE_VOLUME_DOWN,
+            KeyEvent.KEYCODE_VOLUME_UP -> super.onKeyDown(keyCode, event).let { true }
+            else -> super.onKeyDown(keyCode, event)
+        }
     }
 
 }
